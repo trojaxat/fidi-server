473886d1ea4f3471b29267b3f0f0b761a08ab21b
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2018-12-08 08:10:16 +0000

    Returning User Icons

--- controllers/loadUserIcons.js
+++ controllers/loadUserIcons.js
@@ -1,15 +1,13 @@
 const handleLoadUserIcons = (req, res, db) => {
     // need to change this for security reasons
     const { email } = req.body;
-    console.log('req.body', req.body);
     db('photos')  
         .where('email', '=', email)
         .select('link')
         .limit(10)
         .bind(console)
         .then(links => {
-        console.log('links', links);
-        res.json(links)
+        return res.json(links)
         }).catch(err => res.status(400).json('Photos not received from database'))
 }
 
