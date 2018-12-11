f3074b66654bf37b4bc6dbe189e88b3c94976880
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2018-12-11 10:14:36 +0000

    LoadUserIcons sends also place info when fetched

--- controllers/loadUserIcons.js
+++ controllers/loadUserIcons.js
@@ -1,9 +1,10 @@
 const handleLoadUserIcons = (req, res, db) => {
     // need to change this for security reasons
+    // change limit 10 to extend the amount of previous photos visible
     const { email } = req.body;
     db('photos')  
         .where('email', '=', email)
-        .select('link')
+        .select('link', 'place')
         .limit(10)
         .bind(console)
         .then(links => {
