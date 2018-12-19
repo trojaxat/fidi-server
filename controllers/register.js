a7f0a62d70e0211fc1fb82a786ee263faf36f523
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2018-12-19 18:25:05 +0000

    Another attempt with postgresql

--- controllers/register.js
+++ controllers/register.js
@@ -7,25 +7,20 @@ const handleRegister = (req, res, db, bcrypt) => {
     if (!email || !username || !password) {
         return  res.status(400).json('One of the fields is empty')
     }
-        db.transaction(trx => {
-          trx.insert({
+        db('users')
+          .insert({
                 username: username,
                 email: email,
                 hash: hash,
                 entries: 0,
                 date: new Date()
           })
-          .into('users')
           .returning('email')
           .then(response => {
             res.json(response);
-              })
-          })
-          .then(trx.commit)
-          .catch(trx.rollback)
-        }.catch(err => res.status(400).json('unable to register')) 
-    }
+        }).catch(err => res.status(400).json('Unable to register')) 
 }
+
 
 
 //db.select('email')
