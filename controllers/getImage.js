2b11b0fb88f1f960bbe465da109c41c06f649596
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2019-01-24 16:07:29 +0100

    Update getImage.js

--- controllers/getImage.js
+++ controllers/getImage.js
@@ -2,12 +2,11 @@ const handleGetImage = (req, res, db) => {
     const { email, name, place } = req.body;
     db('photos')
         .returning('*')
-        .where({
-            email: email,
-            name: name
-        }).orWhere({
-            place: place
-        }).select('link')
+        .where(function() {
+            this.where(email: email)
+        .orWhere(name: name)
+        }).orWhere({place: place})
+        .select('link')
         .then(link => {
         return res.json(link[0])
         }).catch(err => res.status(400).json('Photo not found'))
