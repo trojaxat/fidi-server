e162cd5d8fa43e825edf9baab26105879bbcb61a
trojaxatbuk <38283627+trojaxatbuk@users.noreply.github.com>
2019-02-20 18:32:38 +0100

    Added possibility to find an image based on link

--- controllers/getImage.js
+++ controllers/getImage.js
@@ -1,5 +1,5 @@
 const handleGetImage = (req, res, db) => {
-    const { email, name, place } = req.body;
+    const { email, name, place, link } = req.body;
     db('photos')
         .returning('*')
         .where({
@@ -7,7 +7,9 @@ const handleGetImage = (req, res, db) => {
         }).andWhere({
             name: name
         }).orWhere({
-            place: place
+            place: place       
+        }).orWhere({
+            link: link
         }).then(link => {
         return res.json(link[0])
         }).catch(err => res.status(400).json('Photo not found'))
