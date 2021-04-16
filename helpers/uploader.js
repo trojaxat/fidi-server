34e5c1ec4b48f62136a4d748241c9c8eb8a28845
trojaxat <dan.axford89@gmail.com>
2021-04-17 01:00:52 +0200

    Bugfix for when the same file is uploaded multiple times

--- helpers/uploader.js
+++ helpers/uploader.js
@@ -1,30 +1,28 @@
 // handles the database storage
-const multer = require('multer');
-// for file upload
-var uploadPath = require('path');
+const multer = require("multer");
 
 // Specific audio upload
 const handleStorage = (req, file, callback) => {
-         return multer.diskStorage({
-          destination: function (req, file, callback) {  
-            callback(null, './public/uploads');  
-          },  
-            filename: function(req, file, callback) {
-                uploadPath = uploadPath.extname(file.originalname);
-                var str = file.originalname;
-                var audioType = str.split('.').pop();
-                callback(null, req.body.hash + "." + audioType);
-            }
-        })
-    }
+  return multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, "./public/uploads");
+    },
+    filename: function (req, file, callback) {
+      let uploadPath = require("path");
+      uploadPath = uploadPath.extname(file.originalname);
+      var str = file.originalname;
+      var audioType = str.split(".").pop();
+      callback(null, req.body.hash + "." + audioType);
+    },
+  });
+};
 
 // this has to keep myfile matching to the app information otherwise it doesnt work
 const handleUpload = (storage) => {
-        return multer({ storage : storage}).single('myfile');
-    }
-
+  return multer({ storage: storage }).single("myfile");
+};
 
 module.exports = {
-    handleStorage,
-    handleUpload,
-}
+  handleStorage,
+  handleUpload,
+};
