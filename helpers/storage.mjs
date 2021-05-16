b0a4f0e0b6902916d9960cf9cc1d8fc1fe2cfe9d
Dan <38283627+trojaxat@users.noreply.github.com>
2021-05-16 12:02:30 +0200

    Change path for server

--- helpers/storage.mjs
+++ helpers/storage.mjs
@@ -2,7 +2,7 @@ import uploadPath from "path";
 import __dirname from './dir.mjs';
 
 export default function storageDestination(upload, hash) {
-  let basePath = __dirname + '\\public\\uploads\\';
+  let basePath = __dirname + '/public/uploads/';
   let fileExtension = uploadPath.extname(upload.name);
   return basePath + hash + fileExtension;
 };
